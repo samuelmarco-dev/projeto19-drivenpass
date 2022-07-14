8a75e9632581b9d9fca8a9109a323150df0beeb0
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-07-14 15:55:58 -0300

    feat: handleError update

--- src/middlewares/errorMIddleware.ts
+++ src/middlewares/errorMIddleware.ts
@@ -4,8 +4,13 @@ import chalk from "chalk";
 export default async function handleError(err, req: Request, res: Response, next: NextFunction){
     if(err){
         console.log(chalk.red('error: ', err));
-        if(err.type === 'InvalidPassword') return res.sendStatus(401);
+        if(unauthorized(err)) return res.status(401).send('Unauthorized');
     }
 
     res.sendStatus(500);
+}
+
+function unauthorized(err){
+    const verification = err.type === ('InvalidToken' || 'SessionError' || 'InvalidPassword');
+    return verification;
 }
