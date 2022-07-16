2c74306964f824c793e1f569f05b69b51de53543
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-07-16 12:23:58 -0300

    feat: correção rotas wifi

---
+++ prisma/migrations/20220716151950_correcao_wifi/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "keys_wifi_title_userId_key";
--- src/routers/wifiRouter.ts
+++ src/routers/wifiRouter.ts
@@ -8,8 +8,8 @@ import schemaWifi from "../schemas/schemaWifi.js";
 const wifiRouter = Router();
 
 wifiRouter.post('/wifi', validationSchema(schemaWifi), validationTokenUser, createWifiUser);
-wifiRouter.get('/wifi', validationTokenUser, getWifis);
-wifiRouter.get('/wifi', validationTokenUser, getWifiById);
-wifiRouter.delete('/wifi', validationTokenUser, deleteWifiById);
+wifiRouter.get('/wifis', validationTokenUser, getWifis);
+wifiRouter.get('/wifi/:id', validationTokenUser, getWifiById);
+wifiRouter.delete('/wifi/:id', validationTokenUser, deleteWifiById);
 
 export default wifiRouter;
